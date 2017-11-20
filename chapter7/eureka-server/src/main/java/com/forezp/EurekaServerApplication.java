c5c85479f340f0a6212586fc124008c63193b314
KiplingGit <zhangwei.168520@sohu.com>
2017-11-20 16:12:21 +0800

    高可用的分布式配置中心(Spring Cloud Config)

--- chapter7/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
+++ chapter7/eureka-server/src/main/java/com/forezp/EurekaServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/*
+ * eureka没做负载均衡，只是做了集群的注册
+ */
 @EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
